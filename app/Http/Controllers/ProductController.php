4386f6d50a5ee64f11008865ee365ce0fa54ffd8
ledang046 <ledang046@gmail.com>
2021-10-23 04:22:48 +0700

    admin reply

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -36,12 +36,12 @@ class ProductController extends Controller
     public function loadComment(Request $request)
     {
         $product_id = $request->product_id;
-        $comment = Comment::where("product_id",$product_id)->where('display',1)->get();
+        $comment = Comment::where("product_id",$product_id)->where('display',1)->where('reply_comment_id',"=",null)->get();
+        $replys = Comment::where("reply_comment_id",">",0)->get();
         $output = '';
         foreach($comment as $key => $comm)
         {
-            $output = ' 
-
+            $output.= ' 
             <div class="be-comment">
             <div class="be-img-comment">	
                 <a href="blog-detail-2.html">
@@ -62,11 +62,39 @@ class ProductController extends Controller
                     '. $comm->comment .'
                 </p>
             </div>
-        </div>
+           </div>
             ';
-            echo $output;
+            // Admin
+            foreach($replys as $key  => $rep){
+                if($rep->reply_comment_id == $comm->id){
+                    $output.= '
+                    <div style="width:80%;margin-left:30px;" class="be-comment">
+                    <div class="be-img-comment">	
+                        <a href="blog-detail-2.html">
+                            <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="" class="be-ava-comment">
+                        </a>
+                    </div>
+                    <div class="be-comment-content">
+                        
+                            <span class="be-comment-name">
+                                <a href="blog-detail-2.html">Admin</a>
+                                </span>
+                            <span class="be-comment-time">
+                                <i class="fa fa-clock-o"></i>
+                                '. $rep->date .'
+                            </span>
+        
+                        <ps class="be-comment-text">
+                        '. $rep->comment .'
+                        </p>
+                    </div>
+                   </div>
+                    ';
+                }
+            }
+            //End Admin 
         }
-       
+        echo $output;
     }
 
     public function sendComment(Request $request)
